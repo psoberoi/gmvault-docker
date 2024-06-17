c457e2a8b4a0a4477a0a6d994166d025bed7f5c9
Paramjit Oberoi <psoberoi@gmail.com>
2024-06-17 16:39:32 -0700

    limit email to last 10 lines

--- backup_full.sh
+++ backup_full.sh
@@ -3,8 +3,9 @@
 echo "Starting full sync of $GMVAULT_EMAIL_ADDRESS."
 echo "Report will be sent to $GMVAULT_SEND_REPORTS_TO."
 
-gmvault sync -d /data $GMVAULT_OPTIONS $GMVAULT_EMAIL_ADDRESS 2>&1 \
-	| tee /data/${GMVAULT_EMAIL_ADDRESS}_full.log \
-	| mail -s "Mail Backup (full) | $GMVAULT_EMAIL_ADDRESS | `date +'%Y-%m-%d %r %Z'`" $GMVAULT_SEND_REPORTS_TO
+LOGFILE="/data/${GMVAULT_EMAIL_ADDRESS}_full.log"
+
+gmvault sync -d /data $GMVAULT_OPTIONS $GMVAULT_EMAIL_ADDRESS >"$LOGFILE" 2>&1
+tail -n 10 "$LOGFILE" | mail -s "Mail Backup (full) | $GMVAULT_EMAIL_ADDRESS | `date +'%Y-%m-%d %r %Z'`" $GMVAULT_SEND_REPORTS_TO
 
 echo "Full sync complete."
